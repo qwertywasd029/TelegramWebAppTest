609d560c1bc9829732595512ed0beb71661f1e11
Riccardo-Vaccari <vaccari.riccardo02@gmail.com>
2023-09-18 12:45:53 +0200

    udpate index

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pyrogram import Client
+from pyrogram import Client, idle, types, filters
 
 import config
 import dep_config as config
@@ -10,12 +10,27 @@ bot = Client(
     bot_token=config.BOT_TOKEN
 )
 
+@bot.on_message()
+def process_message(client: Client, message: types.Message) -> None:
+    if message.web_app_data:
+        print(message.web_app_data)
+
 if __name__ == "__main__":
 
     print(f"Starting {config.APP_NAME}")
     try:
         bot.start()
-        bot.send_message(chat_id=247189896, text="Ci sono!")
+        bot.send_message(
+            chat_id=247189896, 
+            text="Ci sono!", 
+            reply_markup=types.InlineKeyboardMarkup(
+                inline_keyboard=[[types.InlineKeyboardButton(
+                    text="Avvia Web App",
+                    web_app=types.WebAppInfo(url="https://qwertywasd029.github.io/TelegramWebAppTest/index.html")
+                )]]
+            )
+        )
+        idle()
         bot.stop()
     except KeyboardInterrupt:
         pass
